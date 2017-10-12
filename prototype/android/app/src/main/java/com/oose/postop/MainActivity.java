5edff5fc2ce4221f677004c4b1a01b3a638e6f73
Rohit Aakash <5477233+rohitaakash@users.noreply.github.com>
2017-10-11 23:00:48 -0400

    Adding prototype

---
+++ prototype/android/app/src/main/java/com/oose/postop/MainActivity.java
@@ -0,0 +1,31 @@
+package com.oose.postop;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText email, password;
+    Button login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+
+//        login.setOnClickListener()
+     }
+
+}
