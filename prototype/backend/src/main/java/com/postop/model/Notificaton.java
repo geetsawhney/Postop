34b3dd108bd3eaabf2fefc9bb86405b6e6cccad4
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2017-10-15 01:45:33 -0400

    models are updated

---
+++ prototype/backend/src/main/java/com/postop/model/Notificaton.java
@@ -0,0 +1,9 @@
+package com.postop.model;
+
+public class Notificaton {
+    private int notificationId;
+    private int patientId;
+    private  String notificationMessage;
+    private String notificationType;
+    private boolean isResponded;
+}
