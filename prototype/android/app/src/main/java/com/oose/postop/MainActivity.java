9c8cf9af34ed991a09ed099e779ea46de11bdd49
Omotola <omotolaakeredolu@yahoo.com>
2017-10-19 10:30:05 -0400

    Obtain device ID from GCM

--- prototype/android/app/src/main/java/com/oose/postop/MainActivity.java
+++ prototype/android/app/src/main/java/com/oose/postop/MainActivity.java
@@ -1,9 +1,16 @@
 package com.oose.postop;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -11,6 +18,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,18 +30,91 @@ public class MainActivity extends AppCompatActivity {
     EditText emailField;
     EditText ssnField;
     String deviceId;
-
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Creating a broadcast receiver for gcm registration
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize all fields
-        mTextView = (TextView)findViewById(R.id.output);
-        emailField = (EditText)findViewById(R.id.email);
-        ssnField = (EditText)findViewById(R.id.ssn);
-        deviceId ="211312ekjkuhiu1h21U3u2";
+        //Initializing our broadcast receiver
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            //When the broadcast received
+            //We are sending the broadcast from GCMRegistrationIntentService
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                //If the broadcast has received with success
+                //that means device is registered successfully
+                if (intent.getAction().equals(RegistrationIntentService.REGISTRATION_SUCCESS)) {
+                    //Getting the registration token from the intent
+                    String token = intent.getStringExtra("token");
+                    Toast.makeText(getApplicationContext(), "Registration token:" + token, Toast.LENGTH_LONG).show();
+                    setContentView(R.layout.activity_main);
+
+                    // Initialize all fields
+                    mTextView = (TextView) findViewById(R.id.output);
+                    emailField = (EditText) findViewById(R.id.email);
+                    ssnField = (EditText) findViewById(R.id.ssn);
+                    deviceId = token;
+                } else if (intent.getAction().equals(RegistrationIntentService.REGISTRATION_ERROR)) {
+                    Toast.makeText(getApplicationContext(), "GCM registration error!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error occurred", Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
+        //Checking play service is available or not
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+
+        //if play service is not available
+        if (ConnectionResult.SUCCESS != resultCode) {
+            //If play service is supported but not installed
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                //Displaying message that play service is not installed
+                Toast.makeText(getApplicationContext(), "Google Play Service is not install/enabled in this device!", Toast.LENGTH_LONG).show();
+                GooglePlayServicesUtil.showErrorNotification(resultCode, getApplicationContext());
+
+                //If play service is not supported
+                //Displaying an error message
+            } else {
+                Toast.makeText(getApplicationContext(), "This device does not support for Google Play Service!", Toast.LENGTH_LONG).show();
+            }
+
+            //If play service is available
+        } else {
+            //Starting intent to register device
+            Intent itent = new Intent(this, RegistrationIntentService.class);
+            startService(itent);
+        }
     }
+
+    //Registering receiver on activity resume
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity", "onResume");
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(RegistrationIntentService.REGISTRATION_SUCCESS));
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(RegistrationIntentService.REGISTRATION_ERROR));
+    }
+
+
+    //Unregistering receiver on activity paused
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w("MainActivity", "onPause");
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+    }
+
+
 
     /**
      * Onclick listener for login button
