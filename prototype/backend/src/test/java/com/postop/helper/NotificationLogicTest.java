a88455544ac1a2971c4dacd392f91118b0de28ba
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2017-11-01 00:03:06 -0400

    adding test cases for NotificationLogic

--- prototype/backend/src/test/java/com/postop/helper/NotificationLogicTest.java
+++ prototype/backend/src/test/java/com/postop/helper/NotificationLogicTest.java
@@ -3,12 +3,25 @@ package com.postop.helper;
 import org.testng.annotations.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class NotificationLogicTest {
 
     @Test
     public void testSomething(){
-        assertEquals("True", "True");
+
+        assertEquals(new NotificationLogic().ageStatus("H",60),"M");
+    }
+
+    @Test
+    public void  testNotificationLogic(){
+
+        assertEquals(new NotificationLogic().ageStatus("L",20), "L");
+    }
+
+    @Test
+    public void testShouldFail(){
+        assertNotEquals(new NotificationLogic().ageStatus("L",20), "M");
     }
 
 }
