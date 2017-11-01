8a7e0c901ba558e7651f8a86c4ee999ce5ee1918
Rohit Aakash <5477233+rohitaakash@users.noreply.github.com>
2017-10-31 23:23:54 -0400

    Adding basic unit test

---
+++ prototype/backend/src/test/java/com/postop/helper/NotificationLogicTest.java
@@ -0,0 +1,14 @@
+package com.postop.helper;
+
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NotificationLogicTest {
+
+    @Test
+    public void testSomething(){
+        assertEquals("True", "True");
+    }
+
+}
