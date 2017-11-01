04d2aceed0f9245b2abc96cdb2f2a926f7f464ee
Rohit Aakash <5477233+rohitaakash@users.noreply.github.com>
2017-10-31 21:50:25 -0400

    added basic test folder

---
+++ prototype/backend/test/com/postop/utils/NotificationLogicTest.java
@@ -0,0 +1,14 @@
+package com.postop.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NotificationLogicTest {
+
+    @Test
+    public void testSomething(){
+        assertEquals("True", "True");
+    }
+
+}
