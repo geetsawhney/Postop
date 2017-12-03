73b31c9c8d2161807a9f6c97551c09695a195e62
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2017-12-03 03:53:40 -0500

    Updated JSON transformer by defining date formats for Gson to stop changing the dates directly

--- prototype/backend/src/main/java/com/postop/utils/JsonTransformer.java
+++ prototype/backend/src/main/java/com/postop/utils/JsonTransformer.java
@@ -2,6 +2,7 @@
 package com.postop.utils;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import spark.Response;
 import spark.ResponseTransformer;
 
@@ -9,7 +10,7 @@ import java.util.HashMap;
 
 public class JsonTransformer implements ResponseTransformer {
 
-    private Gson gson = new Gson();
+    private Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
 
     @Override
     public String render(Object model) {
