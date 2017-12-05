3f81eefad0259ae128de83140c98d31bc19f40d7
gvijh1 <gvijh92@gmail.com>
2017-12-04 20:52:05 -0500

    Introduced checks for new patient fields, update fields and resolved update bugs

--- postop/backend/src/main/resources/public/js/post-server.js
+++ postop/backend/src/main/resources/public/js/post-server.js
@@ -6,9 +6,100 @@ function getBooleanFromCheckValue(val) {
     console.log(dict[val])
     return dict[val]
 }
+function checkValues()
+{
+    var errorInit
+    var finalErrors="Check "
+    var reEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    var emailCheck = reEmail.test($("#cb-email").val())
+    if(emailCheck==0)
+    {
+      errorInit="Email"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+    var reGender = /M|F/
+    var genderCheck = reGender.test($("#cb-sex").val())
+    if(genderCheck==0)
+    {
+      errorInit=", Gender"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+    var reDOB= /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/
+    var dobCheck = reDOB.test($("#cb-date").val())
+    if(dobCheck==0)
+    {
+      errorInit=", Date of Birth"
+      finalErrors=finalErrors.concat(errorInit)
+    }
 
+    var reLastVisitDate= /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/
+    var lastVisitDateCheck = reLastVisitDate.test($("#cb-last-visit-date").val())
+    if(lastVisitDateCheck==0)
+    {
+      errorInit=", Last Visit Date"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+    addressCheck=1
+    if(!$("#cb-address").val())
+    {
+      addressCheck=0
+      errorInit=", Address"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+
+    hospitalVisitReasonCheck=1
+    if(!$("#cb-hospital-visit-reason").val())
+    {
+      hospitalVisitReasonCheck=0
+      errorInit=", Hospital Visit Reason"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+
+    passwordCheck=1
+    if(!$("#cb-password").val())
+    {
+      passwordCheck=0
+      errorInit=", Password"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+
+    var reName= /^[a-z ,.'-]+$/i
+    var nameCheck = reName.test($("#cb-name").val())
+    if(nameCheck==0)
+    {
+      errorInit=", Name"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+    var reUtiVisitCount= /[0-9]+/
+    var utiVisitCountCheck = reUtiVisitCount.test($("#cb-uti-visit-count").val())
+    if(utiVisitCountCheck==0)
+    {
+      errorInit=", UTI Visit Count"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+
+    var rePhone= /[0-9]{10}/
+    var phoneCheck = rePhone.test($("#cb-phone").val())
+    if(phoneCheck==0)
+    {
+      errorInit=", Phone"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+
+    if(emailCheck==1 && genderCheck==1 && phoneCheck==1 && dobCheck==1 && lastVisitDateCheck==1 && addressCheck==1 && hospitalVisitReasonCheck==1 && utiVisitCountCheck==1 && passwordCheck==1 && nameCheck==1 )
+    {
+      return 1;
+    }
+    else {
+      alert(finalErrors)
+    }
+}
 function submitPatient() {
+
     $("#submit-patient-btn").click(function() {
+      var checkVal = checkValues();
+      if(checkVal==1)
+      {
         var data = {
             name:$("#cb-name").val(),
             email:$("#cb-email").val(),
@@ -21,22 +112,10 @@ function submitPatient() {
             phone:$("#cb-phone").val(),
             hospitalVisitReason:$("#cb-hospital-visit-reason").val(),
             utiVisitCount:$("#cb-uti-visit-count").val(),
-            //catheterUsage: true,
-            //diabetic: true,
            catheterUsage: getBooleanFromCheckValue($("#cb-uses-catheter").val()),
           diabetic: getBooleanFromCheckValue($("#cb-is-diabetic").val()),
             lastVisitDate:$("#cb-last-visit-date").val()
           }
-
-            /*email: $("#cb-email").val(),
-            callbackDate: $("#cb-date").val(),
-            severity: parseInt($("#cb-severity").val()),
-            hasPain: getBooleanFromCheckValue($("#cb-has-pain").val()),
-            hasNausea: getBooleanFromCheckValue($("#cb-has-nausea").val()),
-            hasFever: getBooleanFromCheckValue($("#cb-has-fever").val()),
-            hasFatigue: getBooleanFromCheckValue($("#cb-has-fatigue").val()),
-            isResolved: getBooleanFromCheckValue($("#cb-is-resolved").val()),
-            urineColor: $("#cb-urine-color").val()*/
 
         console.log(data)
 
@@ -47,12 +126,14 @@ function submitPatient() {
             contentType: 'application/json',
             success: function(data) {
                 alert('Post success')
+                location.reload();
             }
         });
         // postRequest.error(function() {
         //     alert('Error')
         // })
-    })
+    }})
+
 }
 
 $(document).ready(function() {
--- postop/backend/src/main/resources/public/js/updatePatient.js
+++ postop/backend/src/main/resources/public/js/updatePatient.js
@@ -1,6 +1,106 @@
+function getBooleanFromCheckValue(val) {
+    var dict = {
+        "on": true,
+        "off": false
+    }
+    console.log(dict[val])
+    return dict[val]
+}
+
+function checkValues()
+{
+    var errorInit
+    var finalErrors="Check "
+    var reEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    var emailCheck = reEmail.test($("#up-email").val())
+    if(emailCheck==0)
+    {
+      errorInit="Email"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+    var reGender = /M|F/
+    var genderCheck = reGender.test($("#up-sex").val())
+    if(genderCheck==0)
+    {
+      errorInit=", Gender"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+    var reDOB= /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/
+    var dobCheck = reDOB.test($("#up-date").val())
+    if(dobCheck==0)
+    {
+      errorInit=", Date of Birth"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+
+    var reLastVisitDate= /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/
+    var lastVisitDateCheck = reLastVisitDate.test($("#up-last-visit-date").val())
+    if(lastVisitDateCheck==0)
+    {
+      errorInit=", Last Visit Date"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+    addressCheck=1
+    if(!$("#up-address").val())
+    {
+      addressCheck=0
+      errorInit=", Address"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+
+    hospitalVisitReasonCheck=1
+    if(!$("#up-hospital-visit-reason").val())
+    {
+      hospitalVisitReasonCheck=0
+      errorInit=", Hospital Visit Reason"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+
+    passwordCheck=1
+    if(!$("#up-password").val())
+    {
+      passwordCheck=0
+      errorInit=", Password"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+
+    var reName= /^[a-z ,.'-]+$/i
+    var nameCheck = reName.test($("#up-name").val())
+    if(nameCheck==0)
+    {
+      errorInit=", Name"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+    var reUtiVisitCount= /[0-9]+/
+    var utiVisitCountCheck = reUtiVisitCount.test($("#up-uti-visit-count").val())
+    if(utiVisitCountCheck==0)
+    {
+      errorInit=", UTI Visit Count"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+
+    var rePhone= /[0-9]{10}/
+    var phoneCheck = rePhone.test($("#up-phone").val())
+    if(phoneCheck==0)
+    {
+      errorInit=", Phone"
+      finalErrors=finalErrors.concat(errorInit)
+    }
+
+    if(emailCheck==1 && genderCheck==1 && phoneCheck==1 && dobCheck==1 && lastVisitDateCheck==1 && addressCheck==1 && hospitalVisitReasonCheck==1 && utiVisitCountCheck==1 && passwordCheck==1 && nameCheck==1 )
+    {
+      return 1;
+    }
+    else {
+      alert(finalErrors)
+    }
+}
+
 function updatePatient() {
     console.log("here!")
-    //$("#update-patient-btn").click(function() {
+      var checkVal = checkValues();
+      if(checkVal==1)
+      {
       console.log("inside update patient")
         var data = {
             name:$("#up-name").val(),
@@ -13,9 +113,9 @@ function updatePatient() {
             phone:$("#up-phone").val(),
             hospitalVisitReason:$("#up-hospital-visit-reason").val(),
             utiVisitCount:5,
-           catheterUsage:false,
-           diabetic:false,
-            lastVisitDate:"2017-10-12"
+           catheterUsage:getBooleanFromCheckValue($("#up-uses-catheter").val()),
+           diabetic:getBooleanFromCheckValue($("#up-is-diabetic").val()),
+            lastVisitDate:$("#up-last-visit-date").val()
           }
         console.log(data)
         var target=$("#up-email").val()
@@ -28,10 +128,10 @@ function updatePatient() {
             contentType: 'application/json',
             success: function(data)
             {
-              location.reload();              
+              location.reload();
             }
         });
-    //})
+  }  //})
 }
 
 $(document).ready(function() {
