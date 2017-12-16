84bcdee6f73aa95899247a41c110676f01861d8e
Rohit Aakash <5477233+rohitaakash@users.noreply.github.com>
2017-12-16 02:48:17 -0500

    Testing mail failure in travis

--- postop/backend/src/main/java/com/postop/utils/MailUtil.java
+++ postop/backend/src/main/java/com/postop/utils/MailUtil.java
@@ -24,12 +24,17 @@ public class MailUtil {
         final String password = "oose2017group19";
 
         Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
+//        props.put("mail.smtp.host", "smtp.gmail.com");
+//        props.put("mail.smtp.socketFactory.port", "465");
+//        props.put("mail.smtp.socketFactory.class",
+//                "javax.net.ssl.SSLSocketFactory");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.port", "465");
+
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
 
         Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
